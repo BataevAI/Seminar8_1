edb3f33d3694b1a4c7c5b74cec56992690a0020d
Andrey Bataev <bataevai@gmail.com>
2023-03-19 14:17:59 +0400

    phonebook

--- spravochnik.py
+++ spravochnik.py
@@ -1,4 +1,5 @@
 # программа по работе со справочником
+import re
 TEXTFILE = 'file.txt'
 def phonebook_read():
     with open(TEXTFILE, encoding='UTF-8') as file:
@@ -12,6 +13,33 @@ def contact_add():
         if len(input_fio) <= lenght_fio: 
             input_fio = '' + input_fio + (lenght_fio - len(input_fio)) * ' '          
             file.write(f'{input_fio} | {input_phone}\n')
+def string_add(lst):
+    ...
+
+# функция выделения ФИО и номера телефона из общей строки справочника
+
+def import_words(str_1) -> list:
+
+    str_1 = str_1.replace('|', ' ')    
+    lst_0 = (re.split(' +', str_1))
+    return list(lst_0)
+
+# функция заменты элемента списка по номеру
+
+def change_words2(lst, item, new_item): 
+    dict_new = dict(enumerate(lst))
+    list_02 = list(map(lambda v: new_item if v is item else dict_new[v], dict_new))
+    return list_02
+
+def change_words3(lst, item, new_item) -> dict: 
+    dict_new = dict(enumerate(lst))
+    list_02 = list(map(lambda v: new_item if v is item else dict_new[v], dict_new))
+    return dict(enumerate(list_02))
+
+def change_list_to_dict(lst) -> dict: 
+    dict_new = dict(enumerate(lst))
+    return dict_new
+    
 
 
 def contact_search():
@@ -30,35 +58,61 @@ def contact_search():
         if flag: 
             print('Ничего не найдено!\n')
         return lst_2
+    
+def print_dict(lst):
+    for i in lst:
+        print(f'номер элемента: {i}, элемент: {lst[i]}')   
+
+# Создание строки из dict по формату справочника
+def string_add(lst):
+    lenght_fio = 30
+    input_fio = ''
+    for i in range(len(lst) - 1):
+        input_fio = input_fio + lst.get(i) + ' '
+    input_phone = lst.get(len(lst) - 1)
+
+    if len(input_fio) <= lenght_fio:
+        input_fio = '' + input_fio + (lenght_fio - len(input_fio)) * ' ' 
+        new_str = input_fio + ' | ' + input_phone
+    return new_str  
 
 def contact_change():
     with open(TEXTFILE, encoding='UTF-8') as file:
-        print('    ')
+        
         search = input('Введите ФИО или телефон для корректировки контакта\n')
         file_search = file.read().split('\n')
-        flag = True
+        
         lst_2 = []
-        lst_3 = []
+        
         for i in file_search:
             
             if search.lower() in i.lower():
-                # print('    ')
-                print(i)                
-                lst_2.append(i)
-                flag = False 
-            if flag: 
-                print('Ничего не найдено!\n')
-        
-        print("Введите цифру строки, которую будем менять")
-        enum = enumerate(lst_2)
-        for id, item in enum:
-            print(id, item)
+                                
+                lst_2.append(i)                
 
-        num_of_str = int(input())
-        for id, item in enum:
-            if num_of_str == id:
-                print(item)
-   
+    if len(lst_2) == 1:
+        lst_2 = change_list_to_dict(import_words(str(*lst_2)))
+        print_dict(lst_2)
+        new_num = int(input('Введите номер элемента, который хотите изменить:\n'))
+        new_word = input('Введите элемент, который хотите изменить:\n')
+        lst_2[new_num] = new_word
+        lst_2 = string_add(lst_2)
+        print(f'Строка справочника после изменений будет выглядеть:\n {lst_2} \n')
+         
+    elif len(lst_2) > 1:
+        lst_2 = change_list_to_dict(lst_2)
+        print_dict(lst_2)        
+        new_num = int(input('Введите номер строки, которую хотите изменить:\n'))
+        lst_2 = change_list_to_dict(import_words(lst_2.get(new_num)))
+        print_dict(lst_2)
+        new_num1 = int(input('Введите номер элемента, которое хотите изменить:\n'))
+        new_word1 = input('Введите элемент, который хотите изменить:\n')
+        lst_2[new_num1] = new_word1
+        lst_2 = string_add(lst_2)
+        print(f'Строка справочника после изменений будет выглядеть так:\n {lst_2} \n')
+    elif len(lst_2) == 0:
+        print('Ничего не найдено!\n')
+
 
 def main():
   
--- test.py
+++ test.py
@@ -1,10 +1,71 @@
 
 import re
-str_1 =  'Baffie                         | 12445'
+TEXTFILE = 'file.txt'
+str_3 =  'Baffie  kot       Bara                | 12445'
+# str_4 =  'Gringo Valley Skott            | 345'
+lst_5 = ['Gringo', 'Valley', 'Skott', '345', '345345353454']
+# print(dict(enumerate(lst_5)))
+# функция выделения ФИО и номера телефона из общей строки справочника
+
+def import_words(str_1) -> list:
+
+    str_1 = str_1.replace('|', ' ')    
+    lst_0 = (re.split(' +', str_1))
+    return list(lst_0)
+
+# print(import_words(str_4))
 
 
-b = str_1.split()
-b = ''.join(b)
-c = b.split('|')
-for id, item in enumerate(c):
-    print(id, item)
+def change_words1(lst, item, new_item) -> dict:
+    dict_1 = dict(enumerate(lst))
+    for i in dict_1:
+        if i is item:
+            dict_1[i] = new_item      
+    return dict_1
+    
+def change_words(lst, item, new_item) -> list:
+
+    list_01 = list(map(lambda t: new_item if t is item else t, lst))
+    return list_01
+
+def change_words2(lst, item, new_item) -> dict: 
+    dict_new = dict(enumerate(lst))
+    list_02 = list(map(lambda v: new_item if v is item else dict_new[v], dict_new))
+    return dict(enumerate(list_02))
+
+# print(change_words2(import_words(str_3), 0, 'Batanaaev'))
+
+
+dict_2 = {0: 'Terminator', 1: 'T800', 2: '0000'}
+lst_5 = [dict_2.get(i) for i in dict_2]
+# print(lst_5)
+
+dict_3 = {0: 'Rambo John                     | 111',
+1: 'Joan Linsky                    | 564',
+2: 'John Sm                        | 124', 
+3: 'Smith John                     | 111'}
+
+# print(dict_3[2])
+
+dict_4 = {0: 'Rambo', 1: 'John', 2: '111'}
+# new_num1 = 0
+# new_word1 = 'Rob'
+
+# dict_4[new_num1] = new_word1
+# print(dict_4)
+
+def string_add(lst):
+    lenght_fio = 30
+    input_fio = ''
+    for i in range(len(lst) - 1):
+        input_fio = input_fio + lst.get(i) + ' '
+    input_phone = lst.get(len(lst) - 1)
+
+    if len(input_fio) <= lenght_fio:
+        input_fio = '' + input_fio + (lenght_fio - len(input_fio)) * ' ' 
+        new_str = input_fio + ' | ' + input_phone
+    return new_str 
+
+print(len(dict_4))
+print(dict_4.get(0))
+print(string_add(dict_4))
